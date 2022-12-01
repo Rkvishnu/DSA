e6ebd2639a5a84ad442979433daa34a6ab3faab3
vishnu <vr8043836@gmail.com>
2022-12-02 00:03:02 +0530

    day20

---
+++ recursion-backT/myPow.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+		// Optimal Solution Time O(logN) & Auxiliary Space O(logN)
+        if(n==0) 
+			return 1;
+		 
+        double y = myPow(x,n/2);
+        if(n%2==0)
+            return y*y;
+        else if(n < 0)
+            return 1/x*y*y;
+        else
+            return x*y*y; 
+    }
+};
